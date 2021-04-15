c2267b1b27ee06f339bae92069e124b2d0f07f3a
Prachi <prachi10160shukla@gmail.com>
2021-04-15 20:42:02 +0530

    new_map added

--- map/urls.py
+++ map/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('', views.map, name='map'),
+    #path('', views.map, name='map'),
+    path('', views.new_map, name='new_map'),
+
 ]
--- map/views.py
+++ map/views.py
@@ -3,8 +3,11 @@ from home.models import PictureLocation
 from django.core import serializers
 
 # Create your views here.
-def map(request):
-    qs = PictureLocation.objects.all()
-    serialized = serializers.serialize("json", qs)
-    print(serialized[0])
-    return render(request,'map/map.html',{'data':qs})
+#def map(request):
+ #   qs = PictureLocation.objects.all()
+    #serialized = serializers.serialize("json", qs)
+   # print(serialized[0])
+  #  return render(request,'map/map.html',{'data':qs})
+
+def new_map(request):
+    return render(request,"map/new_map.html",{})
