0920c7f1178ac46b27a0f41fbf0ab5492250e6ae
Prachi <prachi10160shukla@gmail.com>
2021-05-22 14:58:22 +0530

    debug1

--- forms/urls.py
+++ forms/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('basicinfo', views.basicForm, name='basicForm'),
-    path('nginfo', views.NGForm, name='NGForm'),
-    path('factsheet', views.factsheet, name='factsheet'),
-    path('factsheet_dynamic', views.factsheet_dynamic, name='factsheet_dynamic'),
-    path('financial', views.financial, name='financial'),
-    path('output_indicators', views.output_indicators, name='output_indicators'),
+    path('basicinfo', views.basicinfo, name='basicinfo'),
+    # path('nginfo', views.NGForm, name='NGForm'),
+    # path('factsheet', views.factsheet, name='factsheet'),
+    # path('factsheet_dynamic', views.factsheet_dynamic, name='factsheet_dynamic'),
+    # path('financial', views.financial, name='financial'),
+    # path('output_indicators', views.output_indicators, name='output_indicators'),
 ]
