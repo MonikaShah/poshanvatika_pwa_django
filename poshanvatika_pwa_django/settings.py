17ae07133fe0c3ccb6c9a5d32ddbb410182ea79a
AnimeshN <animesh.nauti@gmail.com>
2021-06-06 20:43:54 +0530

    layer updated

--- poshanvatika_pwa_django/settings.py
+++ poshanvatika_pwa_django/settings.py
@@ -127,7 +127,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'poshan',
         'USER' : 'postgres',
-        'PASSWORD' : 'postgres1713000',
+        'PASSWORD' : 'postgres',
         'HOST' : 'localhost',
         'PORT' : '5432',
     }
