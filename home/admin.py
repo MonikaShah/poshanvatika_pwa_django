9ef3bef08c58797bb1bc14c7888503e715919361
Admin <renukasnaik19@gmail.com>
2022-02-28 16:30:05 +0530

    CensusTable model added to admin

--- home/admin.py
+++ home/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import PoshanFormInformation, UploadPictureModel, UploadWellPictureModel
+from .models import PoshanFormInformation, UploadPictureModel, UploadWellPictureModel, CensusTable
 admin.site.register(UploadPictureModel)
 admin.site.register(UploadWellPictureModel)
 admin.site.register(PoshanFormInformation)
+admin.site.register(CensusTable)
